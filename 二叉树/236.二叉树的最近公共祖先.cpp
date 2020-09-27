6dee69df3286ac0c082f3231f4d3b77d44399b46
Lijinliang Niu <niulijinliang000@gmail.com>
2020-09-27 10:00:13 +0800

    2020/9/27

---
+++ 二叉树/236.二叉树的最近公共祖先.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=236 lang=cpp
+ *
+ * [236] 二叉树的最近公共祖先
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == nullptr){
+            return nullptr;
+        }
+        if(root == p || root == q){
+            return root;
+        }
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if(left != nullptr && right != nullptr){
+            return root;
+        }
+        else if(left == nullptr){
+            return right;
+        }
+        else{
+            return left;
+        }
+    }
+};
+// @lc code=end
+
