a12cef77c72ff43bf2b18d60285fdf217d957dc7
Lijinliang Niu <niulijinliang000@gmail.com>
2020-12-09 11:05:47 +0800

    2020/12/9

---
+++ 链表/21.合并两个有序链表.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=cpp
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode(-1);
+        ListNode* tail = head;
+        while(l1 != nullptr && l2 != nullptr){
+            if(l1->val < l2->val){
+                tail->next = l1;
+                l1 = l1->next;
+            }
+            else{
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+        tail->next = (l1 == nullptr) ? l2 : l1;
+        tail = head;
+        head = head->next;
+        delete tail;
+        return head;
+    }
+};
+// @lc code=end
+
